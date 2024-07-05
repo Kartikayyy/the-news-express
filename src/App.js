ee5c89f71ba697a2b67816c8893a207bb98e00c2
Kartikayyy <kartikaygupta2808@gmail.com>
2024-07-05 19:06:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoadingBar from "react-top-loading-bar";
 
-function App() {
+let apiKey = process.env.REACT_APP_NEWS_API;
+console.log(process.env.REACT_APP_NEWS_API);
+const App = () => {
+  const paths = [
+    "",
+    "business",
+    "entertainment",
+    "general",
+    "health",
+    "science",
+    "sports",
+    "technology",
+    "keywordSearch:id",
+  ];
+  let [progress, setProgress] = useState(0);
+
+  // const setPProgress = (progress) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <NavBar />
+        <LoadingBar height={3} color="#f11946" progress={progress} />
+
+        <Routes>
+          {paths.map((path, ind) => {
+            return (
+              <Route
+                exact
+                key={ind}
+                path={`/${path}`}
+                element={
+                  <News
+                    apiKey={apiKey}
+                    setProgress={setProgress}
+                    key={ind}
+                    pageSize={12}
+                    country="in"
+                    category={path}
+                    keyword={""}
+                  />
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;

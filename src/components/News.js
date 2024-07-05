81481fc4dd74a91559bb6632f6387bacd8894e8d
Kartikayyy <kartikaygupta2808@gmail.com>
2024-07-05 19:44:08 +0530

    first commit

--- src/components/News.js
+++ src/components/News.js
@@ -15,27 +15,43 @@ const News = (props) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  const updateNews = async () => {
-    props.setProgress(10);
+  // const updateNews = async () => {
+  //   props.setProgress(10);
     
-    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&q=${props.keyword}&category=${props.category}&apiKey=15a5d5de56314d7ba3c60f61e17c3049&page=${page}&pagesize=${props.pageSize}`;
-    setloading(true);
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResult(parsedData.totalResults);
-    setloading(false);
-    props.setProgress(100);
-  };
+  //   let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&q=${props.keyword}&category=${props.category}&apiKey=15a5d5de56314d7ba3c60f61e17c3049&page=${page}&pagesize=${props.pageSize}`;
+  //   setloading(true);
+  //   let data = await fetch(url);
+  //   props.setProgress(30);
+  //   let parsedData = await data.json();
+  //   props.setProgress(70);
+  //   setArticles(parsedData.articles);
+  //   setTotalResult(parsedData.totalResults);
+  //   setloading(false);
+  //   props.setProgress(100);
+  // };
+  
 
   useEffect(() => {
+    const updateNews = async () => {
+      props.setProgress(10);
+      
+      let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&q=${props.keyword}&category=${props.category}&apiKey=15a5d5de56314d7ba3c60f61e17c3049&page=${page}&pagesize=${props.pageSize}`;
+      setloading(true);
+      let data = await fetch(url);
+      props.setProgress(30);
+      let parsedData = await data.json();
+      props.setProgress(70);
+      setArticles(parsedData.articles);
+      setTotalResult(parsedData.totalResults);
+      setloading(false);
+      props.setProgress(100);
+    };
+
     document.title = `${
       !props.category ? "HOME" : capitalizeFirstLetter(props.category)
     } - The News Express`;
     updateNews();
-  }, [props.country, props.keyword, props.category, props.pageSize]);
+  },[props.country, props.keyword, props.category, props.pageSize]);
 
   const fetchMoreData = async () => {
     setPage(page + 1);
